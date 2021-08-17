6e90eb4addf36f1165375c8aa3205f64857020be
Muaz Wazir <muazwazir@gmail.com>
2021-08-18 03:16:17 +0800

    upate default login route

--- learn-spring-security/src/main/java/com/example/learnspringsecurity/config/SecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/example/learnspringsecurity/config/SecurityConfiguration.java
@@ -28,18 +28,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // Changing default route for login from "/login" to "/api/login"
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Permit path without auth :- to permit all for a specific endpoints
         // Routes such as "/login" is being handled by Spring boot & Spring Security
-        http.authorizeRequests().antMatchers("").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
 
         // Setting permissions for guarded routes
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAuthority("user");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/save/**").hasAuthority("admin");
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Bean @Override
