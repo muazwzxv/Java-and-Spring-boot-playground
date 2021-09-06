1182956d4cb1dcc3efdb892c46393a6a6fc1ebaf
Muaz Wazir <muazwazir@gmail.com>
2021-09-06 16:25:42 +0800

    Upate

---
+++ learn-spring-security/src/main/java/com/example/learnspringsecurity/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.example.learnspringsecurity.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
--- learn-spring-security/src/main/java/com/example/learnspringsecurity/exception/EmailNotFoundException.java
+++ learn-spring-security/src/main/java/com/example/learnspringsecurity/exception/EmailNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.learnspringsecurity.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.persistence.EntityNotFoundException;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EmailNotFoundException extends EntityNotFoundException {
     public EmailNotFoundException(String email) {
         super("Email " + email + " is not found");
